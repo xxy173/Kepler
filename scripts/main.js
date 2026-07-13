8eb8f1cb90cef6f3e2cf5c6eaa34e36833f48eb4
xxy173 <kinki0913@gmail.com>
2026-07-13 22:09:58 +0900

    a

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
-let u = 1
+let coolingTester = extend(GenericCrafter, "cooling-tester", {});
+
+coolingTester.health = 200;
+coolingTester.size = 2;
+coolingTester.requiredCooling = 10;
+
+coolingTester.buildType = function(){
+  return extend(GenericCrafter.GenericCrafterBuild, coolingTester, {
+    cooling: 0,
+    wasCooled: false,
+
+    updateTile(){
+      this.super$updateTile();
+
+      let isCooled = this.cooling >= this.block.requiredCooling;
+
+      if(isCooled !== this.wasCooled){
+        if(isCooled){
+          print("冷却テスター: 冷却OK");
+        }else{
+          print("冷却テスター: 冷却不足");
+        }
+
+        this.wasCooled = isCooled;
+      }
+    }
+  });
+};
