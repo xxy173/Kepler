b64786466a67f016b7c22721b3fe46833f035ef7
xxy173 <kinki0913@gmail.com>
2026-07-18 22:50:12 +0900

    a

--- scripts/block.js
+++ scripts/block.js
@@ -1,23 +1,33 @@
 const steelMill = extend(GenericCrafter, "steel-mill", {
-    hasItems: true,
-    hasLiquids: true,
-    hasPower: true,
-    itemCapacity: 20,
-    liquidCapacity: 20,
-    size: 3,
+    init(){
+        const steel = Vars.content.getByName(ContentType.item, "kepler-steel");
+        const iron = Vars.content.getByName(ContentType.item, "kepler-iron");
+
+        this.consumeItems(
+            ItemStack.with(
+                iron, 1,
+                Items.coal, 1
+            )
+        );
+        this.consumePower(1);
+        this.craftTime = 60;
+        this.outputItem = new ItemStack(steel, 1);
+
+        this.super$init();
+    }
 });
 
-steelMill.requirements(
-  Category.crafting,
-  ItemStack.with(
-    Items.copper, 30,
-    Items.lead, 15
-  )
-);
+steelMill.buildType = () => extend(GenericCrafter.GenericCrafterBuild, steelMill, {
+    getProgressIncrease(baseTime){
+        const hasWater = this.liquids.get(liquids.water) > 0;
+        return this.super$getProgressIncrease(baseTime) * (hasWater ? 2 : 1);
+    },
+    updateTile(){
+        this.super$updateTile();
+        const water = this.liquids.get(liquids.water);
+        if(water > 0 && this.efficiency > 0){
+            this.liquids.remove(liquids.water, Math.min(water, 0.1))
+        }
+    }
+});
 
-steelMill.consumeItem(Items.lead, 1);
-steelMill.consumeLiquid(Liquids.water, 0.1);
-steelMill.consumePower(1);
-
-steelMill.craftTime = 60;
-steelMill.outputItem = new ItemStack(Items.copper, 1);
