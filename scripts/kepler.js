ebab5f07b1bc7d4ea8fac6d9aff6c31395690b6f
xxy173 <kinki0913@gmail.com>
2026-07-21 21:07:49 +0900

    a

--- scripts/kepler.js
+++ scripts/kepler.js
@@ -1,4 +1,11 @@
+const iron = Vars.content.getByName(contentType.item, "kepler-iron");
 const steel = Vars.content.getByName(ContentType.item, "kepler-steel");
-const iron = Vars.content.getByName(contentType.item, "kepler-iron")
 const oxygen = Vars.content.getByName(ContentType.liquid, "kepler-oxygen");
 
+print(iron + steel + oxygen)
+
+module.exports = {
+    iron: iron,
+    steel: steel,
+    oxygen: oxygen
+}
