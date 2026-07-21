d850c59c8d807136ff2c7a474018ec5e526a3ad7
xxy173 <154735753+xxy173@users.noreply.github.com>
2026-07-21 12:55:06 +0900

    kepler.js の作成

---
+++ scripts/kepler.js
@@ -0,0 +1,4 @@
+const steel = Vars.content.getByName(ContentType.item, "kepler-steel");
+const iron = Vars.content.getByName(contentType.item, "kepler-iron")
+const oxygen = Vars.content.getByName(ContentType.liquid, "kepler-oxygen");
+
